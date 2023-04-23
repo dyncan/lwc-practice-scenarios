b91f1a9880695d62c52bb15ec98a129474bf7117
Peter Dong <dynckm@gmail.com>
2023-04-23 13:14:13 +0800

    update scratch org config file

--- install-dev.sh
+++ install-dev.sh
@@ -13,7 +13,7 @@ sfdx force:org:delete -p -u $ORG_ALIAS &> /dev/null
 echo ""
 
 echo "Creating scratch org..." && \
-sfdx force:org:create -s -f config/project-scratch-def.json -d 1 -a $ORG_ALIAS && \
+sfdx force:org:create -s -f config/project-scratch-def.json -d 7 -a $ORG_ALIAS && \
 echo "" && \
 
 echo "Pushing source..." && \
